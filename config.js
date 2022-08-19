b6f55775909626a84fd6927194fcff47ddea8f2d
Bomiro <105790086+Bomiro@users.noreply.github.com>
2022-08-19 22:58:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ const chalk = require("chalk");
 // Other
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983";
+  gg = "212705570169";
 }
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "none";
 global.pgdb = process.env.DATABASE_URL || "none";
 global.port = process.env.PORT || 8080;
-(global.packname = process.env.PACKNAME || "kaguya"),
+(global.packname = process.env.PACKNAME || "𝘽𝙤𝙢𝙞𝙧𝙤 😁"),
   (global.prefix = process.env.PREFIX || "-"),
-  (global.author = process.env.AUTHOR || "ARUS"),
+  (global.author = process.env.AUTHOR || "fin hny 🙂"),
   (global.sessionName = "arus"),
-  (global.name = process.env.NAME || "Mizuhara"),
+  (global.name = process.env.NAME || "𝐍𝐀𝐌𝐗𝟐𝟏𝟐𝐁𝐎𝐓"),
   (global.thumb = fs.readFileSync("./trash/info.jpeg"));
 
 let file = require.resolve(__filename);
